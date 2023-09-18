060125ae437775e7998c58f2638e42fbd92834cf
Haziqbatak <haziqimtiyaz17@gmail.com>
2023-09-18 20:28:24 +0700

    ds

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ window.onscroll = () => {
 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
-};
+};  
 
     /*=================== scroll reveal ====================*/
     ScrollReveal({
